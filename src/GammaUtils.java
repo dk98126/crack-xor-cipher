80b1ffdc4a3ee117235d3c65d03cfc76a228e231
Daniil Krasnov <dk98126@yandex.ru>
2019-11-27 19:26:49 +0300

    GammaUtils added, lsr file still unstable

---
+++ src/GammaUtils.java
@@ -0,0 +1,23 @@
+public class GammaUtils {
+    public static char[] getXoredChars(char[] text, char[] gammaPart, final int offset, final int gammaLength) {
+        if (gammaPart.length + offset > gammaLength) {
+            throw new IllegalArgumentException("Указанная длина для гаммы меньше суммы смещения и длины предпологаемой гаммы:\n " +
+                    gammaLength + " < " + (gammaPart.length + offset));
+        }
+        char[] pseudoGamma = new char[gammaLength];
+        for (int i = 0; i < offset; i++) {
+            pseudoGamma[i] = 0;
+        }
+        for (int i = offset, j = 0; i < gammaPart.length + offset; i++, j++) {
+            pseudoGamma[i] = gammaPart[j];
+        }
+        for (int i = gammaPart.length + offset; i < gammaLength; i++) {
+            pseudoGamma[i] = 0;
+        }
+        char[] resultedText = new char[text.length];
+        for (int i = offset; i < text.length; i++) {
+            resultedText[i] = (char) (text[i] ^ pseudoGamma[i % gammaLength]);
+        }
+        return resultedText;
+    }
+}
--- src/LRS.java
+++ src/LRS.java
@@ -1,22 +1,39 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 public class LRS {
+    public static final int OFFSET = 2;
+    public static final int GAMMA_LENGTH = 9;
 
     public static void main(String[] args) {
-        String bytes = null;
+        String charsInBytes = null;
         try (BufferedReader reader = new BufferedReader(new FileReader("txt.txt"))) {
-            bytes = reader.readLine();
+            charsInBytes = reader.readLine();
         } catch (IOException e) {
             System.out.println(e.getMessage());
             System.exit(1);
         }
-        if (bytes.length() % 2 != 0) {
+        if (charsInBytes.length() % 2 != 0) {
             throw new IllegalArgumentException("Неверная длина текста");
         }
-        List<String> strings = StringUtils.mostOftenStrings(bytes, 2, 4);
+        char[] realChars = StringUtils.getCharsArrayFromBytesString(charsInBytes);
+        String realCharsString = String.valueOf(realChars);
+        List<String> repeatedStrings = StringUtils.mostOftenStrings(realCharsString, 2, 4);
+        String gammaPart = Collections.max(repeatedStrings);
+        String gammaPartInBytes = StringUtils.getBytesStringFromCharsArray(gammaPart.toCharArray());
+        char[] gammaPartChars = StringUtils.getCharsArrayFromBytesString(gammaPartInBytes);
+        for (int i = 0; i < 50; i++) {
+            for (int j = 0; gammaPart.length() + j <= i; j++) {
+                String decipheredText = String.valueOf(GammaUtils.getXoredChars(realChars, gammaPartChars, j, i));
+                String decipheredTextBytes = StringUtils.getBytesStringFromCharsArray(String.valueOf(GammaUtils.getXoredChars(realChars, gammaPartChars, j, i)).toCharArray());
+                if (decipheredTextBytes.contains("0000000000")) {
+                    System.out.println();
+                }
+            }
+        }
         System.out.printf("");
     }
 }
